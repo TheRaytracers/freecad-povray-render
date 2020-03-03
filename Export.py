77fc082d4bde54df7ef59bf6716c24392384569b
Usb Hub <usbhub.spam@web.de>
2020-03-03 15:06:32 +0100

    Add all for export command

--- Export.py
+++ Export.py
@@ -1,3 +1,22 @@
+# -*- coding: utf-8 -*-
+
+#  Export FreeCAD models to POV-Ray
+#  Copyright (C) 2019  Usb Hub and DerUhrmacher
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 2.1 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
 import FreeCAD as App
 import FreeCADGui as Gui
 
@@ -17,6 +36,9 @@ class Export:
 			return True
 
 	def Activated(self):
-		App.Console.PrintWarning("Start Export")
+		from Dialog import Dialog
+		
+		dialog = Dialog()
+		dialog.exec_()
 
 Gui.addCommand('Export', Export())
---
+++ helpDefs.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+#  Export FreeCAD models to POV-Ray
+#  Copyright (C) 2019  Usb Hub and DerUhrmacher
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 2.1 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+import FreeCAD as App
+import FreeCADGui as Gui
+
+from PySide import QtCore, QtGui
+
+def showError(text, title): #show an error box
+    dialog = QtGui.QMessageBox(QtGui.QMessageBox.Critical, title, text)
+    dialog.setWindowModality(QtCore.Qt.ApplicationModal)
+    dialog.exec_()
+
+def isAscii(string): #test whether the string is an ASCII string
+    return all(ord(c) < 128 for c in string)
+
+def stringCorrection(inputString): # String correction for compatibility with POV-Ray 
+        utfString = inputString.encode("utf8", "replace")     # conversion from ??? to utf8
+        uniString = utfString.decode("utf8", "replace")       # conversion from utf8 to unicode
+        firstchar = ord(uniString[0])                         # POV-Ray doesn't like numbers as first charakter
+        if firstchar >= 48 and firstchar <= 57:
+            uniString = "_" + uniString
+        uniString = uniString.replace(u"Ä", "Ae")             # replacement of german "Umlaute"
+        uniString = uniString.replace(u"ä", "ae")
+        uniString = uniString.replace(u"Ü", "Ue")
+        uniString = uniString.replace(u"ü", "ue")
+        uniString = uniString.replace(u"Ö", "Oe")
+        uniString = uniString.replace(u"ö", "oe")
+        uniString = uniString.replace(u"ß", "ss")
+        uniString = uniString.replace(u"(", "_")
+        uniString = uniString.replace(u")", "_")
+        uniString = uniString.replace(u"#", "_")
+        uniString = uniString.replace(u".", "_")
+        uniString = uniString.replace(u",", "_")
+        uniString = uniString.replace(u"-", "_")
+        uniString = uniString.replace(u"+", "_")
+        uniString = uniString.replace(u"*", "_")
+        uniString = uniString.replace(u"/", "_")
+        uniString = uniString.replace(" ", "_")
+        outString = uniString.encode("ASCII", "replace")      # conversion to ASCII for POV-Ray compatibility
+        return outString.decode("utf-8")                      #return and redecode to unicode but with converted chars
+
+def strToBool(str):
+    if str == True:
+        return True
+    elif str == False:
+        return False
+
+    return str.lower() in ['true', '1', 'y']
