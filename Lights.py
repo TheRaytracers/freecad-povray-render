f86cdf0d2bab977aa1b3a06fd78d67107f0ea1c5
Usb Hub <usbhub.spam@web.de>
2020-03-10 14:53:09 +0100

    Use better viewprovider for pointLight

--- Lights.py
+++ Lights.py
@@ -1,6 +1,7 @@
 import FreeCAD as App
 import FreeCADGui as Gui
 import os
+from pivy import coin
 
 class PointLight:
     def __init__(self, obj):
@@ -19,29 +20,54 @@ class PointLight:
         return True
 
 class ViewProviderPointLight:
-    def __init__(self, vobj):
-        vobj.Proxy = self
+    def __init__(self, obj):
+        '''Set this object to the proxy object of the actual view provider'''
+        obj.Proxy = self
+ 
+    def attach(self, obj):
+        '''Setup the scene sub-graph of the view provider, this method is mandatory'''
+        self.noShadow = coin.SoGroup()
 
-    def attach(self, vobj):
-        self.Object = vobj.Object
-
-    def __getstate__(self):
-        return None
-
-    def __setstate__(self, state):
-        return None
-
-    def getDisplayModes(self, vobj):
-        return ["Default"]
-
+        img = coin.SoImage()
+        img.filename = os.path.join(os.path.dirname(__file__),"icons","pointLight.svg")
+        self.noShadow.addChild(img)
+        obj.addDisplayMode(self.noShadow, "Default")
+ 
+    def updateData(self, fp, prop):
+        '''If a property of the handled feature has changed we have the chance to handle this here'''
+        pass
+ 
+    def getDisplayModes(self,obj):
+        '''Return a list of display modes.'''
+        modes=[]
+        modes.append("Default")
+        return modes
+ 
     def getDefaultDisplayMode(self):
+        '''Return the name of the default display mode. It must be defined in getDisplayModes.'''
         return "Default"
-
-    def setDisplayMode(self, mode):
+ 
+    def setDisplayMode(self,mode):
+        '''Map the display mode defined in attach with those defined in getDisplayModes.\
+                Since they have the same names nothing needs to be done. This method is optional'''
         return mode
-
-    def isShow(self):
-        return True
-
+ 
+    def onChanged(self, vp, prop):
+        '''Here we can do something when a single property got changed'''
+        pass
+ 
     def getIcon(self):
+        '''Return the icon in XPM format which will appear in the tree view. This method is\
+                optional and if not defined a default icon is shown.'''
         return os.path.join(os.path.dirname(__file__),"icons","pointLight.svg")
+ 
+    def __getstate__(self):
+        '''When saving the document this object gets stored using Python's json module.\
+                Since we have some un-serializable parts here -- the Coin stuff -- we must define this method\
+                to return a tuple of all serializable objects or None.'''
+        return None
+ 
+    def __setstate__(self,state):
+        '''When restoring the serialized object from document we have the chance to set some internals here.\
+                Since no data were serialized nothing needs to be done here.'''
+        return None
