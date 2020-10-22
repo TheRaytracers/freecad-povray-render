977e8b52f8096494e006ada242fd96d1e16e1c4a
Usb Hub <usbhub.spam@web.de>
2020-10-22 17:43:34 +0200

    Block opening of multiple dialog windows

--- ExportCommand.py
+++ ExportCommand.py
@@ -23,6 +23,8 @@ import FreeCADGui as Gui
 class ExportCommand:
 	"Open dialog and start export"
 
+	dialogOpened = False
+
 	def GetResources(self):
 		return {"MenuText": "Export Model",
 				"Accel": "Ctrl+E",
@@ -30,15 +32,17 @@ class ExportCommand:
 				"Pixmap"  : "logo.svg"}
 
 	def IsActive(self):
-		if App.ActiveDocument == None:
+		if App.ActiveDocument == None or self.dialogOpened:
 			return False
 		else:
 			return True
 
 	def Activated(self):
+		self.dialogOpened = True
 		from Dialog import Dialog
 		
 		dialog = Dialog()
 		dialog.exec_()
+		self.dialogOpened = False
 
 Gui.addCommand('Export', ExportCommand())
